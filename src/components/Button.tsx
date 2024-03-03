374263faa8c81a3c8900c51874dbc05eb6267360
abizek <91976421+abizek@users.noreply.github.com>
2024-03-03 13:02:35 +0530

    hover styles for button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -4,9 +4,10 @@ import { cn } from '../utils'
 const buttonVariants = cva('min-w-28 min-h-11 scale-95 rounded-full', {
   variants: {
     variant: {
-      default: 'bg-indigo-500 text-white',
-      secondary: 'bg-gray-300/60 disabled:bg-gray-200 text-gray-700 disabled:text-gray-500',
-      destructive: 'bg-red-700/90 text-white',
+      default: 'bg-indigo-500 text-white hover:bg-indigo-500/90',
+      secondary:
+        'bg-gray-300/60 active:bg-gray-300/60 hover:bg-gray-200 disabled:bg-gray-200 text-gray-700 disabled:text-gray-500',
+      destructive: 'bg-red-700/90 hover:bg-red-700/80 text-white',
     },
   },
   defaultVariants: {
@@ -18,12 +19,8 @@ interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {}
 
-export function Button({
-  className,
-  variant,
-  ...props
-}: ButtonProps) {
+export function Button({ className, variant, ...props }: ButtonProps) {
   return (
-    <button className={cn(buttonVariants({ variant, className }))} {...props}/>
+    <button className={cn(buttonVariants({ variant, className }))} {...props} />
   )
 }
