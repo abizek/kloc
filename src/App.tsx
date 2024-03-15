b02d173a862dbc977d237b6d7f03eb3d4634576e
abizek <91976421+abizek@users.noreply.github.com>
2024-03-15 16:06:02 +0530

    add timer tab

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,19 @@ import { Stopwatch } from './pages/Stopwatch'
 export default function App() {
   return (
     <Tabs defaultValue="stopwatch">
-      <TabsContent value="kloc">
+      <TabsContent value="kloc" data-cy="kloc">
         <Kloc />
       </TabsContent>
-      <TabsContent value="stopwatch">
+      <TabsContent value="stopwatch" data-cy="stopwatch">
         <Stopwatch />
+      </TabsContent>
+      <TabsContent value="timer" data-cy="timer">
+        Coming soon
       </TabsContent>
       <TabsList>
         <TabsTrigger value="kloc">Kloc</TabsTrigger>
         <TabsTrigger value="stopwatch">Stopwatch</TabsTrigger>
+        <TabsTrigger value="timer">Timer</TabsTrigger>
       </TabsList>
     </Tabs>
   )
