50cf9fd1b1818bd7edf00a39812d867a36e3d098
abizek <91976421+abizek@users.noreply.github.com>
2024-03-25 13:39:38 +0530

    persist timer input values in local storage

--- src/components/TimerInput.tsx
+++ src/components/TimerInput.tsx
@@ -1,47 +1,74 @@
 import { useMotionValueEvent, useScroll } from 'framer-motion'
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 
 type TimerInputProps = {
   label: string
   values: string[]
   onChange: (value: string) => void
+  persistedValue: string
+  hideSeparator?: boolean
 }
 
 type ValueItemProps = {
   value: string
   containerRef: React.RefObject<HTMLDivElement>
   onChange: (value: string) => void
+  persistedValue: string
 }
 
-export function TimerInput({ label, values, onChange }: TimerInputProps) {
+export function TimerInput({
+  label,
+  values,
+  onChange,
+  persistedValue,
+  hideSeparator = false,
+}: TimerInputProps) {
   const ref = useRef<HTMLDivElement>(null)
 
   return (
-    <div>
-      {label}
-      <div
-        ref={ref}
-        className="relative h-72 snap-y snap-mandatory overflow-y-scroll text-3xl no-scrollbar"
-      >
-        {values.map((value) => (
-          <ValueItem
-            key={value}
-            value={value}
-            containerRef={ref}
-            onChange={onChange}
-          />
-        ))}
+    <div className="dark:text-white">
+      <div className="mb-4">{label}</div>
+      <div className="flex items-center gap-4 text-2xl">
+        <div
+          ref={ref}
+          className="relative h-64 snap-y snap-mandatory overflow-y-scroll text-3xl no-scrollbar"
+        >
+          {values.map((value) => (
+            <ValueItem
+              key={value}
+              value={value}
+              containerRef={ref}
+              onChange={onChange}
+              persistedValue={persistedValue}
+            />
+          ))}
+        </div>
+        {!hideSeparator && ':'}
       </div>
     </div>
   )
 }
 
-function ValueItem({ value, containerRef, onChange }: ValueItemProps) {
-  const ref = useRef<HTMLDivElement>(null)
+function ValueItem({
+  value,
+  containerRef,
+  onChange,
+  persistedValue,
+}: ValueItemProps) {
+  const ref = useRef<HTMLDivElement>(null!)
   const { scrollYProgress } = useScroll({
     container: containerRef,
     target: ref,
+    layoutEffect: false,
   })
+
+  useEffect(() => {
+    if (value === persistedValue) {
+      // initial scroll to the persisted value
+      ref.current.scrollIntoView({ behavior: 'instant', block: 'center' })
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
   useMotionValueEvent(scrollYProgress, 'change', (latest) => {
     if (latest === 0.5) {
--- src/pages/Timer.tsx
+++ src/pages/Timer.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useLocalStorage } from '@uidotdev/usehooks'
 import { prefixZero } from '../utils'
 import { timerMachine } from '../machines/timer'
 import { TimerTimeView } from '../components/TimeView'
@@ -7,15 +7,15 @@ import { TimerInput } from '../components/TimerInput'
 import { usePersistedMachine } from '../hooks/usePersistedMachine'
 
 export function Timer() {
-  const [hours, setHours] = useState('00')
-  const [minutes, setMinutes] = useState('00')
-  const [seconds, setSeconds] = useState('00')
+  const [hours, setHours] = useLocalStorage('timer-input-hours', '00')
+  const [minutes, setMinutes] = useLocalStorage('timer-input-minutes', '00')
+  const [seconds, setSeconds] = useLocalStorage('timer-input-seconds', '00')
   const [timer, send] = usePersistedMachine('timer', timerMachine)
 
   return (
     <div className="relative z-10 grid size-full grid-rows-[auto_80px] place-items-center">
       {timer.matches('stopped') ? (
-        <div className="flex gap-4">
+        <div className="flex items-center gap-4">
           <TimerInput
             label="Hours"
             values={Array(100)
@@ -24,6 +24,7 @@ export function Timer() {
             onChange={(value) => {
               setHours(value)
             }}
+            persistedValue={hours}
           />
           <TimerInput
             label="Minutes"
@@ -33,6 +34,7 @@ export function Timer() {
             onChange={(value) => {
               setMinutes(value)
             }}
+            persistedValue={minutes}
           />
           <TimerInput
             label="Seconds"
@@ -42,6 +44,8 @@ export function Timer() {
             onChange={(value) => {
               setSeconds(value)
             }}
+            persistedValue={seconds}
+            hideSeparator
           />
         </div>
       ) : (
