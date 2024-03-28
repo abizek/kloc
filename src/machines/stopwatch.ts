cee64d0fcd4b127b9d6c219e21fabab152d18bcb
abizek <91976421+abizek@users.noreply.github.com>
2024-03-28 14:50:47 +0530

    refactor stopwatch machine

--- src/machines/stopwatch.ts
+++ src/machines/stopwatch.ts
@@ -27,39 +27,46 @@ export const stopwatchMachine = setup({
     events: {} as StopwatchEvents,
   },
   actions: {
-    setElapsed: assign(({ context }) => ({
-      elapsed: Date.now() - context.origin,
-    })),
-    setOrigin: assign(({ context }) => ({
-      origin: Date.now() - context.elapsed,
-    })),
-    setLapOrigin: assign(({ context }) => ({
-      lapOrigin: Date.now() - context.lapElapsed,
-    })),
-    addLap: assign(({ context }) => ({
-      laps: [
-        {
-          id: nanoid(),
-          elapsed:
-            context.laps.length > 0 ? context.lapElapsed : context.elapsed || 0,
-          overall: context.elapsed,
-        },
-        ...context.laps,
-      ],
-    })),
-    resetLapStopwatch: assign(() => ({
-      lapOrigin: Date.now(),
-      lapElapsed: 0,
-    })),
-    setLapElapsed: assign(({ context }) => ({
-      lapElapsed: Date.now() - context.lapOrigin,
-    })),
     resetStopwatch: assign({
       origin: 0,
       elapsed: 0,
       lapOrigin: 0,
       lapElapsed: 0,
       laps: [],
+    }),
+    setOrigin: assign(({ context }) => ({
+      origin: Date.now() - context.elapsed,
+    })),
+    setElapsed: assign(({ context }) => ({
+      elapsed: Date.now() - context.origin,
+    })),
+    setLapOrigin: assign(({ context }) => ({
+      lapOrigin: Date.now() - context.lapElapsed,
+    })),
+    setLapElapsed: assign(({ context }) => ({
+      lapElapsed: Date.now() - context.lapOrigin,
+    })),
+    addFirstLap: assign(({ context }) => ({
+      laps: [
+        {
+          id: nanoid(),
+          elapsed: context.elapsed,
+          overall: context.elapsed,
+        },
+      ],
+    })),
+    addLap: assign(({ context }) => ({
+      laps: [
+        {
+          id: nanoid(),
+          elapsed: context.lapElapsed,
+          overall: context.elapsed,
+        },
+        ...context.laps,
+      ],
+    })),
+    resetLapElapsed: assign({
+      lapElapsed: 0,
     }),
   },
   guards: {
@@ -77,91 +84,51 @@ export const stopwatchMachine = setup({
   initial: 'stopped',
   states: {
     stopped: {
-      on: {
-        start: {
-          target: 'started',
-          actions: {
-            type: 'setOrigin',
-          },
-        },
-      },
+      on: { start: { target: 'started' } },
+      entry: { type: 'resetStopwatch' },
     },
     started: {
       type: 'parallel',
       on: {
-        pause: {
-          target: 'paused',
-        },
+        pause: { target: 'paused' },
         jumpstart: [
-          {
-            target: 'started',
-            guard: {
-              type: 'isLapStopwatchStopped',
-            },
-          },
-          {
-            target: '#stopwatch.started.lapStopwatch.running',
-          },
+          { target: 'started', guard: { type: 'isLapStopwatchStopped' } },
+          { target: '#stopwatch.started.lapStopwatch.started' },
         ],
       },
+      entry: { type: 'setOrigin' },
       states: {
         mainStopwatch: {
           initial: 'running',
-          on: {
-            lap: [
-              {
-                target: '#stopwatch.started.lapStopwatch.running',
-                actions: [
-                  {
-                    type: 'addLap',
-                  },
-                  {
-                    type: 'setLapOrigin',
-                  },
-                ],
-                guard: {
-                  type: 'isLapStopwatchStopped',
-                },
-              },
-              {
-                target: '#stopwatch.started.lapStopwatch.running',
-                actions: [
-                  {
-                    type: 'addLap',
-                  },
-                  {
-                    type: 'resetLapStopwatch',
-                  },
-                ],
-              },
-            ],
-          },
           states: {
             running: {
-              after: {
-                '10': {
-                  target: 'running',
-                  actions: {
-                    type: 'setElapsed',
-                  },
-                  reenter: true,
-                },
-              },
+              after: { '10': { target: 'running', reenter: true } },
+              entry: { type: 'setElapsed' },
             },
           },
         },
         lapStopwatch: {
           initial: 'stopped',
           states: {
-            stopped: {},
-            running: {
-              after: {
-                '10': {
-                  target: 'running',
-                  actions: {
-                    type: 'setLapElapsed',
-                  },
+            stopped: {
+              on: {
+                lap: { target: 'started', actions: { type: 'addFirstLap' } },
+              },
+            },
+            started: {
+              initial: 'running',
+              on: {
+                lap: {
+                  target: 'started',
+                  actions: [{ type: 'addLap' }, { type: 'resetLapElapsed' }],
                   reenter: true,
+                },
+              },
+              entry: { type: 'setLapOrigin' },
+              states: {
+                running: {
+                  after: { '10': { target: 'running', reenter: true } },
+                  entry: { type: 'setLapElapsed' },
                 },
               },
             },
@@ -172,33 +139,10 @@ export const stopwatchMachine = setup({
     paused: {
       on: {
         resume: [
-          {
-            target: 'started',
-            actions: {
-              type: 'setOrigin',
-            },
-            guard: {
-              type: 'isLapStopwatchStopped',
-            },
-          },
-          {
-            target: '#stopwatch.started.lapStopwatch.running',
-            actions: [
-              {
-                type: 'setLapOrigin',
-              },
-              {
-                type: 'setOrigin',
-              },
-            ],
-          },
+          { target: 'started', guard: { type: 'isLapStopwatchStopped' } },
+          { target: '#stopwatch.started.lapStopwatch.started' },
         ],
-        reset: {
-          target: 'stopped',
-          actions: {
-            type: 'resetStopwatch',
-          },
-        },
+        reset: { target: 'stopped' },
       },
     },
   },
