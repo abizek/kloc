93c24bdb7401d29478922f28c1be73640f081b3a
abizek <abishekilango@protonmail.com>
2024-04-21 20:40:59 +0530

    test routes

--- cypress/e2e/root.cy.ts
+++ cypress/e2e/root.cy.ts
@@ -1,33 +1,54 @@
 describe('Root', () => {
   beforeEach(() => {
-    cy.clock()
     cy.visit('/')
   })
 
-  it('tabs', () => {
+  describe('Tab Switch', () => {
     const tabs = [
-      { tab: 'kloc', label: 'Kloc' },
-      { tab: 'stopwatch', label: 'Stopwatch' },
-      { tab: 'timer', label: 'Timer' },
+      { tab: 'kloc', label: 'Kloc', route: '/' },
+      { tab: 'stopwatch', label: 'Stopwatch', route: '/stopwatch' },
+      { tab: 'timer', label: 'Timer', route: '/timer' },
     ]
 
-    tabs.forEach(({ tab, label }, _, tabs) => {
-      cy.get(`[data-cy="${tab}-trigger"]`).contains(label).click()
-      cy.get(`[data-cy="${tab}-trigger"]`)
-        .should('have.attr', 'data-state')
-        .and('match', /active/)
-      cy.get(`[data-cy="${tab}-content"]`)
-        .should('have.attr', 'data-state')
-        .and('match', /active/)
-      tabs
-        .filter(({ tab: otherTab }) => tab !== otherTab)
-        .forEach(({ tab }) => {
-          cy.get(`[data-cy="${tab}-trigger"]`)
-            .should('have.attr', 'data-state')
-            .and('match', /inactive/)
-          cy.get(`[data-cy="${tab}-content"]`)
-            .should('not.exist')
-        })
+    it('via trigger', () => {
+      tabs.forEach(({ tab, label, route }, _, tabs) => {
+        cy.get(`[data-cy="${tab}-trigger"]`).contains(label).click()
+        cy.get(`[data-cy="${tab}-trigger"]`)
+          .should('have.attr', 'data-state')
+          .and('match', /active/)
+        cy.get(`[data-cy="${tab}-content"]`)
+          .should('have.attr', 'data-state')
+          .and('match', /active/)
+        cy.location('pathname').should('eq', route)
+        tabs
+          .filter(({ tab: otherTab }) => tab !== otherTab)
+          .forEach(({ tab }) => {
+            cy.get(`[data-cy="${tab}-trigger"]`)
+              .should('have.attr', 'data-state')
+              .and('match', /inactive/)
+            cy.get(`[data-cy="${tab}-content"]`).should('not.exist')
+          })
+      })
+    })
+
+    it('via route', () => {
+      tabs.forEach(({ tab, route }, _, tabs) => {
+        cy.visit(route)
+        cy.get(`[data-cy="${tab}-trigger"]`)
+          .should('have.attr', 'data-state')
+          .and('match', /active/)
+        cy.get(`[data-cy="${tab}-content"]`)
+          .should('have.attr', 'data-state')
+          .and('match', /active/)
+        tabs
+          .filter(({ tab: otherTab }) => tab !== otherTab)
+          .forEach(({ tab }) => {
+            cy.get(`[data-cy="${tab}-trigger"]`)
+              .should('have.attr', 'data-state')
+              .and('match', /inactive/)
+            cy.get(`[data-cy="${tab}-content"]`).should('not.exist')
+          })
+      })
     })
   })
 })
