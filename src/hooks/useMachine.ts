8915cd63cb5de17765fbb2debe57d94e5fa11d9d
abizek <abishekilango@protonmail.com>
2024-05-01 10:32:52 +0530

    make useMachine custom hook to override context and state on demand

--- src/hooks/useMachine.ts
+++ src/hooks/useMachine.ts
@@ -1,24 +1,105 @@
-import { useMachine as useMachineRaw } from '@xstate/react'
 import { isEmpty, isEqual, omit } from 'lodash'
-import { useCallback, useEffect, useMemo } from 'react'
-import type { Actor, AnyStateMachine, EventFromLogic, StateFrom } from 'xstate'
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+  useSyncExternalStore,
+} from 'react'
+import { createActor } from 'xstate'
+import type {
+  Actor,
+  AnyStateMachine,
+  EventFromLogic,
+  StateFrom,
+  ActorOptions,
+} from 'xstate'
 
 export function useMachine<TMachine extends AnyStateMachine>(
   machine: TMachine,
 ): [StateFrom<TMachine>, Actor<TMachine>['send'], Actor<TMachine>] {
-  const storageKey = useMemo(() => machine.config.id!, [machine.config.id])
-  const persistedSnapshotUnparsed = sessionStorage.getItem(storageKey)
+  const machineId = useMemo(() => machine.config.id!, [machine.config.id])
+
+  const persistedSnapshotUnparsed = sessionStorage.getItem(machineId)
   const persistedSnapshot = useMemo(() => {
     const snapshot = JSON.parse(persistedSnapshotUnparsed ?? 'null')
 
     return { ...snapshot, children: {} } as StateFrom<TMachine>
   }, [persistedSnapshotUnparsed])
 
-  const [snapshot, rawSend, actorRef] = useMachineRaw(machine, {
-    ...(!isEmpty(omit(persistedSnapshot, 'children')) && {
-      snapshot: { ...persistedSnapshot },
-    }),
+  const persist = useCallback(
+    (value: object) => {
+      sessionStorage.setItem(
+        machineId,
+        JSON.stringify(omit(value, ['children', 'historyValue'])),
+      )
+    },
+    [machineId],
+  )
+
+  const [actorRef, setActorRef] = useState(() => {
+    const isSnapshotPersisted = !isEmpty(omit(persistedSnapshot, 'children'))
+    const actorRef = createActor(machine, {
+      ...(isSnapshotPersisted && {
+        snapshot: { ...persistedSnapshot },
+      }),
+    } as ActorOptions<TMachine>)
+
+    if (!isSnapshotPersisted) {
+      persist(actorRef.getPersistedSnapshot())
+    }
+
+    return actorRef
   })
+
+  useEffect(() => {
+    const listener = (event: CustomEvent) => {
+      persist(event.detail)
+      setActorRef(
+        createActor(machine, {
+          ...(!isEmpty(event.detail) && {
+            snapshot: { ...event.detail },
+          }),
+        } as ActorOptions<TMachine>),
+      )
+    }
+    window.addEventListener(`${machineId}-update`, listener as EventListener)
+
+    return () => {
+      window.removeEventListener(
+        `${machineId}-update`,
+        listener as EventListener,
+      )
+    }
+  }, [machine, persist, machineId])
+
+  useEffect(() => {
+    actorRef.start()
+
+    return () => {
+      const snapshot = actorRef.getSnapshot()
+      actorRef.stop()
+
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+      ;(actorRef as any)._snapshot = snapshot
+    }
+  }, [actorRef])
+
+  const getSnapshot = useCallback(
+    () => actorRef.getSnapshot() as StateFrom<TMachine>,
+    [actorRef],
+  )
+
+  const subscribe = useCallback(
+    (onChange: () => void) => {
+      const { unsubscribe } = actorRef.subscribe(onChange)
+      return unsubscribe
+    },
+    [actorRef],
+  )
+
+  const snapshot = useSyncExternalStore(subscribe, getSnapshot)
+  const rawSend = actorRef.send
 
   useEffect(() => {
     // To jumpstart 'always' actions when restoring from persisted state
@@ -26,28 +107,17 @@ export function useMachine<TMachine extends AnyStateMachine>(
   }, [rawSend])
 
   useEffect(() => {
-    if (
-      persistedSnapshot &&
-      !isEqual(snapshot.value, persistedSnapshot.value)
-    ) {
-      sessionStorage.setItem(
-        storageKey,
-        JSON.stringify(
-          omit(actorRef.getPersistedSnapshot(), ['children', 'historyValue']),
-        ),
-      )
+    if (!isEqual(snapshot.value, persistedSnapshot.value)) {
+      persist(actorRef.getPersistedSnapshot())
     }
-  }, [actorRef, persistedSnapshot, snapshot.value, storageKey])
+  }, [actorRef, persist, persistedSnapshot.value, snapshot.value])
 
   const send = useCallback(
     (event: EventFromLogic<TMachine>) => {
       rawSend(event)
-      sessionStorage.setItem(
-        storageKey,
-        JSON.stringify(actorRef.getPersistedSnapshot()),
-      )
+      persist(actorRef.getPersistedSnapshot())
     },
-    [actorRef, rawSend, storageKey],
+    [actorRef, persist, rawSend],
   )
 
   return [snapshot, send, actorRef]
