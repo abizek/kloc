49a6b1c5755f9eb8fd43a74093dbfe93f5875992
abizek <abishekilango@protonmail.com>
2024-05-23 17:44:57 +0530

    switch on off sfx
    Signed-off-by: abizek <abishekilango@protonmail.com>

--- src/client/sounds.ts
+++ src/client/sounds.ts
@@ -3,3 +3,9 @@ beepSfx.loop = true
 
 export const playBeepSfx = () => beepSfx.play()
 export const stopBeepSfx = () => beepSfx.pause()
+
+const switchOnSfx = new Audio('/sounds/switch-on.mp3')
+const switchOffSfx = new Audio('/sounds/switch-off.mp3')
+
+export const playSwitchOnSfx = () => switchOnSfx.play()
+export const playSwitchOffSfx = () => switchOffSfx.play()
