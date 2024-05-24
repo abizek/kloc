2d87e8ce8514787efa871f17cef68d8f554a3547
abizek <abishekilango@protonmail.com>
2024-05-24 13:40:32 +0530

    add create and delete room commands
    Signed-off-by: abizek <abishekilango@protonmail.com>

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -1,3 +1,68 @@
+import PartySocket from 'partysocket'
+
+const stopwatchInitialState = {
+  context: { origin: 0, elapsed: 0, lapOrigin: 0, lapElapsed: 0, laps: [] },
+  status: 'active',
+  value: 'stopped',
+}
+const timerInitialState = {
+  context: { remaining: 0, destination: 0, duration: 0 },
+  status: 'active',
+  value: { stopped: 'beepStopped' },
+}
+
+const createRoom = (room: string) =>
+  new Promise<void>((resolve, reject) => {
+    try {
+      const ws = new PartySocket({
+        host: 'localhost:1999',
+        room,
+        debug: true,
+      })
+
+      ws.addEventListener(
+        'open',
+        () => {
+          ws.send(
+            JSON.stringify({
+              type: 'create',
+              stopwatch: stopwatchInitialState,
+              timer: timerInitialState,
+            }),
+          )
+          ws.close()
+          resolve()
+        },
+        { once: true },
+      )
+    } catch (error) {
+      reject(error)
+    }
+  })
+
+const deleteRoom = (room: string) =>
+  new Promise<void>((resolve, reject) => {
+    try {
+      const ws = new PartySocket({
+        host: 'localhost:1999',
+        room,
+        debug: true,
+      })
+
+      ws.addEventListener(
+        'open',
+        () => {
+          ws.send(JSON.stringify({ type: 'delete' }))
+          ws.close()
+          resolve()
+        },
+        { once: true },
+      )
+    } catch (error) {
+      reject(error)
+    }
+  })
+
 Cypress.Commands.addAll({
   assertTabPresence: (tab: string) => {
     cy.get(`[data-cy="${tab}-trigger"]`)
@@ -15,7 +80,8 @@ Cypress.Commands.addAll({
 
     cy.get(`[data-cy="${tab}-content"]`).should('not.exist')
   },
+  createRoom,
+  deleteRoom,
 })
 
-// XXX: add partysocket create room command
 // XXX: fix handle 404 2nd test, tab switch via trigger with slug, overrides last visited path if specific path is requested
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -4,5 +4,7 @@ declare namespace Cypress {
   interface Chainable {
     assertTabPresence(tab: string): Chainable
     assertTabAbsence(tab: string): Chainable
+    createRoom(room: string): Chainable
+    deleteRoom(room: string): Chainable
   }
 }
